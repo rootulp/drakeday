8c463ccbf7f9ab07efea47a841abce063d32a4f8
Rootul <rootulp@gmail.com>
2014-07-20 00:37:55 -0400

    show lyrics

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,6 +5,7 @@ get '/' do
   songs = RapGenius.search_by_artist("Drake")
   @song = songs[rand(20)]
   @media = @song.media.first
+  @lines = @song.lines
   if @media.provider == 'soundcloud'
      client = Soundcloud.new(:client_id => 'c47cbaa8d972de10824f44f31fd4b529')
      track_url = @media.url
