93579a583d24cc51da3ddec93821acb466aaeb83
Rootul <rootulp@gmail.com>
2014-07-20 00:58:10 -0400

    iterate through media array to grab a soundcloud url

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,13 +3,14 @@ require 'soundcloud'
 
 get '/' do
   songs = RapGenius.search_by_artist("Drake")
+  client = Soundcloud.new(:client_id => 'c47cbaa8d972de10824f44f31fd4b529')
   @song = songs[rand(20)]
-  @media = @song.media.first
   @lines = @song.lines
-  if @media.provider == 'soundcloud'
-     client = Soundcloud.new(:client_id => 'c47cbaa8d972de10824f44f31fd4b529')
-     track_url = @media.url
-    @embed_info = client.get('/oembed', :url => track_url, :auto_play => true)
+  @song.media.each do |media|
+    if media.provider == 'soundcloud'
+      @embed_info = client.get('/oembed', :url => media.url, :auto_play => true, :show_comments => false)
+      break
+    end
   end
 
   erb :index
