d4b10964bd48ce50269bc9707841b8c5434df8a8
Rootul <rootulp@gmail.com>
2014-07-20 00:35:06 -0400

    working soundcloud embeded drake songs

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,13 +5,11 @@ get '/' do
   songs = RapGenius.search_by_artist("Drake")
   @song = songs[rand(20)]
   @media = @song.media.first
-
-  # create a client object with your app credentials
-  client = Soundcloud.new(:client_id => 'c47cbaa8d972de10824f44f31fd4b529')
-
-  # get a tracks oembed data
-  track_url = 'http://soundcloud.com/forss/flickermood'
-  @embed_info = client.get('/oembed', :url => track_url)
+  if @media.provider == 'soundcloud'
+     client = Soundcloud.new(:client_id => 'c47cbaa8d972de10824f44f31fd4b529')
+     track_url = @media.url
+    @embed_info = client.get('/oembed', :url => track_url)
+  end
 
   erb :index
 end
