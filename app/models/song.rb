80280b4529cbd4dcbad498b8acc63a28a80d440a
Rootul <rootulp@gmail.com>
2014-07-31 17:18:56 -0400

    extract track number from soundcloud iframe

--- app/models/song.rb
+++ app/models/song.rb
@@ -31,6 +31,12 @@ class Song
       $client.get('/oembed', :url => url, :auto_play => true, :show_comments => false, :maxwidth => 365, :maxheight =>166)
   end
 
+  def extract_track(html)
+    html_string = html.to_s
+    html_string.match(/tracks%2F(\d*)/) # Grabs just the track number
+    return $1
+  end
+
   def strip_url(url)
     url.match(/\A(\S*\/)(.*\z)/) # Split url into two capture groups - second has song info
     $2.gsub!(/-/, ' ') # Replace dashes with spaces in song title
