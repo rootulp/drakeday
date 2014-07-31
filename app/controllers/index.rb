7a275ee5ea1589b2a924ed4dead75f86926d2704
Rootul <rootulp@gmail.com>
2014-07-31 16:53:32 -0400

    refactor controller

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,11 +7,8 @@ end
 
 get '/:era' do
   song = Song.new
-  rg_song_media = song.rg_song_media
-  rg_song = rg_song_media[0]
-  rg_media = rg_song_media[1]
-  sc_url = rg_media.url
-  lyrics = song.lyrics(rg_song.lines)
-  sc = song.sc(sc_url)
+  rg_song, rg_media =  song.rg_song_media # Set rg_song and rg_media to the song and media info in a rg song
+  sc = song.sc(rg_media.url) # Get embedable SC object using rg media url (a soundcloud link)
+  lyrics = song.lyrics(rg_song.lines) # Take RG lyrics and throw them into one long string
   json_object = {soundcloud_embedable: sc, lyrics: lyrics}.to_json
 end
