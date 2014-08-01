e4d24ce5920f1ce5edff41125372689bdfb49937
Rootul <rootulp@gmail.com>
2014-08-01 14:52:08 -0400

    save current track id in session[:current_track]

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -16,6 +16,14 @@ end
 
 get '/youtube' do
   num_tracks = Track.count
-  rand_track = Track.find_by_id(rand(num_tracks))
+  rand_track_id = rand(num_tracks)
+  session[:current_track] = rand_track_id
+  rand_track = Track.find_by_id(rand_track_id)
   json_object = {youtube_track: rand_track.iframe_info, lyrics: rand_track.lyrics}.to_json
+end
+
+get '/delete' do
+  track_id = session[:current_track]
+  rand_track = Track.find_by_id(track_id)
+  p rand_track
 end
