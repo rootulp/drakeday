f18d23d31ac66ad725d2918cab5702f39719efa9
Rootul <rootulp@gmail.com>
2014-08-01 15:51:22 -0400

    soundcloud routes and stop auto play

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,17 +6,16 @@ get '/' do
 end
 
 get '/sc' do
-  song = Song.new
-  rg_song, rg_media =  song.rg_song_media("soundcloud") # Set rg_song and rg_media to the song and media info in a rg song
-  sc = song.sc(rg_media.url) # Get embedable SC object using rg media url (a soundcloud link)
-  sc_stripped = song.extract_track_sc(sc) # Strips soundcloud embedable for track number to embed in iframe
-  lyrics = song.lyrics(rg_song.lines) # Take RG lyrics and throw them into one long string
-  json_object = {sc_track: sc_stripped, lyrics: lyrics}.to_json
+  num_tracks = Track.count
+  rand_track_id = rand(num_tracks) + 1
+  session[:current_track] = rand_track_id
+  rand_track = Track.find_by_id(rand_track_id)
+  json_object = {sc_track: rand_track.iframe_info, lyrics: "bla"}.to_json
 end
 
 get '/youtube' do
   num_tracks = Track.count
-  rand_track_id = rand(num_tracks)
+  rand_track_id = rand(num_tracks) + 1
   session[:current_track] = rand_track_id
   rand_track = Track.find_by_id(rand_track_id)
   json_object = {youtube_track: rand_track.iframe_info, lyrics: rand_track.lyrics}.to_json
