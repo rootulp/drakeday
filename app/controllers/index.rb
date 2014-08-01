d01140e4bce66bc2b9233cf71e6f43135210549f
Rootul <rootulp@gmail.com>
2014-08-01 16:16:06 -0400

    get random youtube or soundcloud song

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,18 +6,16 @@ get '/' do
 end
 
 get '/sc' do
-  num_tracks = Track.count
-  rand_track_id = rand(num_tracks) + 1
-  session[:current_track] = rand_track_id
-  rand_track = Track.find_by_id(rand_track_id)
+  tracks = Track.where("provider = ?", 'soundcloud')
+  rand_track = tracks.sample
+  session[:current_track] = rand_track.id
   json_object = {sc_track: rand_track.iframe_info, lyrics: "bla"}.to_json
 end
 
 get '/youtube' do
-  num_tracks = Track.count
-  rand_track_id = rand(num_tracks) + 1
-  session[:current_track] = rand_track_id
-  rand_track = Track.find_by_id(rand_track_id)
+  tracks = Track.where("provider = ?", 'youtube')
+  rand_track = tracks.sample
+  session[:current_track] = rand_track.id
   json_object = {youtube_track: rand_track.iframe_info, lyrics: rand_track.lyrics}.to_json
 end
 
