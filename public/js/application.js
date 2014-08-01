a6e598a8e66a9d24abae4a7376b64273f5a771a5
Rootul <rootulp@gmail.com>
2014-08-01 10:47:27 -0400

    hide buttons for 5 seconds and auto recall ajax request if fail - also prep for css spinner

--- public/js/application.js
+++ public/js/application.js
@@ -1,15 +1,21 @@
-console.log("Hello");
 $(document).ready(function() {
-    $("#old").on('click', function() {
+    console.log("Hello");
+    var oldButton = $("#old");
+    var midButton = $("#mid");
+    var highButton = $("#new");
+    $("#old, #mid, #new").on('click', function() {
+        hideButtons();
         ajax_song("old");
-    });  
-    $("#mid").on('click', function() {
-        ajax_song("mid");
-    });  
-    $("#new").on('click', function() {
-        ajax_song("new");
     });
 });
+
+function hideButtons() {
+    $("#old, #mid, #new").slideUp();
+}
+
+function showButtons() {
+    $("#old, #mid, #new").slideDown();
+}
 
 function ajax_song(era) {
     var xhr = $.ajax({
@@ -22,12 +28,15 @@ function ajax_song(era) {
         var lyrics = $("#lyrics");
         var iframeID = $("#soundcloud_iframe");
 
-        iframe_skeleton = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/" + dataParsed.sc_track + "&amp;color=ff5500&amp;auto_play=true&amp;hide_related=true&amp;show_comments=false&amp;show_user=false&amp;show_reposts=false"
+        iframe_skeleton = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/" + dataParsed.sc_track + "&amp;color=ff5500&amp;auto_play=true&amp;hide_related=true&amp;show_comments=false&amp;show_user=false&amp;show_reposts=false";
 
         iframeID.attr("src", iframe_skeleton);
         lyrics.html(dataParsed.lyrics);
+        setTimeout( "showButtons();", 5000 );
+
     }).fail(function(data){
         console.log("Failed");
+         setTimeout( "ajax_song('old');", 5000 );
     });
 }
 
